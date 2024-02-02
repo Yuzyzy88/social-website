2eec8bea5c3152e57ff3a5fb61d6e0669b5826f9
YUzyzy88 <ayu.sulistiowati@protonmail.com>
2024-02-02 18:33:15 +0700

    sharing content on your website

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,8 +1,6 @@
 from  django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     # previous login url
@@ -27,7 +25,3 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
